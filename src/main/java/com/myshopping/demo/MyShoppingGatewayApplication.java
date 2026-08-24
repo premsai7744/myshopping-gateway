d3e554ac6ae8a14e922918ab90a694771586d841
premsai7744 <premsai7744@gmail.com>
2026-08-24 14:12:07 +0530

    Initial commit.

---
+++ src/main/java/com/myshopping/demo/MyShoppingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.myshopping.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShoppingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShoppingGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myshopping/demo/MyShoppingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myshopping.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShoppingGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
